9c4ca784297bc188c402710856247301bec363a4
ioannisvrynios <ioannisvrynios@yahoo.com>
2023-03-07 19:05:37 +0200

    Update product_preview_custom.js

--- source/partials/products/product_preview_custom.js
+++ source/partials/products/product_preview_custom.js
@@ -1,3 +1,3 @@
-if (string.length > 58) {
-  string = string.substring(0, 57) + "...";
+if (product.title.string.length > 58) {
+  product.title.string = product.title.string.substring(0, 57) + "...";
 }
